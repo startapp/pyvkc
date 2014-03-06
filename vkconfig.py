4820b7a2b9e4ee658474e595be020d571df9e3a3
startapp <root@startappka.tk>
2014-03-06 10:33:19 +0200

    My vodka...

--- vkconfig.py
+++ vkconfig.py
@@ -12,8 +12,7 @@ RELAY_PORT = 30047
 
 SHOW_IMAGES = 1
 PROFILE_PHOTO = 'photo_max'
-#Конвертация в Portable Anymap перед показом. В частности для 
-совместимости с WinCE.
+#Конвертация в Portable Anymap перед показом. В частности для совместимости с WinCE.
 USE_PNM = 1
 PNM_TEMP = '.pnm/'
 #PNM_TEMP = '\\Storage Card\\PNM\\' #Для WinCE - полные пути.
